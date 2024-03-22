5f97df6eff08edd1d2ea72f5b53723625fd63f95
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-03-22 13:22:14 +0300

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+// ? Document write ile çıktı alma
+
+// document.write("Hello World <br>") // sayfada yazdırır
+// document.write("İpek Canlı <br>")
+// document.write(25)
+
+// ? Console da çıktı alma
+
+// console.log("Hello World")
+// console.log("İpek Canlı")
+// console.log(3.55)
+// console.log(true)
+// console.log({ name: "İpek", surname: "Canlı" })
+
+// console.clear()  //! sayfayı temizlemek için kullanılır
+
+
+// let a = 5
+// let b = 10
+
+// console.log(a + b)
+
+
+// console.log("İki sayının toplamı :", a + b)
+
+
+// ? Alert Kullanımı  
